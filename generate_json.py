04bdd0439172e09e7625e0ea8e6a177aaef0152a
Tony <antoni97@bk.ru>
2025-04-17 17:56:33 +0200

    loko

--- generate_json.py
+++ generate_json.py
@@ -15,6 +15,19 @@ for file in md_folder.glob("*.md"):
         "title": meta.get("title", ""),
         "tipo": meta.get("tipo", ""),
         "h1": meta.get("h1", ""),
+        "h2t1": meta.get("h2t1", ""),
+        "text1": meta.get("text1", ""),
+        "h2t2": meta.get("h2t2", ""),
+        "text2": meta.get("text2", ""),
+        "h2t3": meta.get("h2t3", ""),
+        "text3": meta.get("text3", ""),
+        "h2t4": meta.get("h2t4", ""),
+        "text4": meta.get("text4", ""),
+        "h2t5": meta.get("h2t5", ""),
+        "text5": meta.get("text5", ""),
+        "h2t6": meta.get("h2t6", ""),
+        "text6": meta.get("text6", ""),
+        "phone": meta.get("phone", ""),
         "images": meta.get("images", [])
     })
 
